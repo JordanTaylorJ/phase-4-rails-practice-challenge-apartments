4b8d9f1b73700314ed7f653591f08a2141421954
JordanJoseph <jordant.joseph@outlook.com>
2022-12-04 15:07:01 -0700

    define routes, migrate tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :leases
-  resources :tenants
-  resources :apartments
+  resources :apartments, only: [:update, :create, :destroy]
+  resources :tenants, only: [:update, :create, :destroy]
+  resource :leases, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
