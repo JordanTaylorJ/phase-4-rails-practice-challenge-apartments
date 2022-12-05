4290638e1ff5049d7030c4c88e11834f46e0191a
JordanJoseph <jordant.joseph@outlook.com>
2022-12-04 21:38:16 -0700

    add validations to tenant, define create and destroy methods in lease_controller

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,2 +1,28 @@
 class LeasesController < ApplicationController
+
+    def create
+        lease = Lease.create(lease_params)
+        if lease.valid? 
+            render json: lease, status: :created
+        else 
+            render json: {errors: lease.errors.full_messages}, status: :unprocessable_entity
+        end 
+    end
+
+    def destroy
+        lease = Lease.find_by(id: params[:id])
+        if lease
+            lease.destroy 
+            head :no_content 
+        else 
+            render json: {error: "lease not found"}, status: :not_found
+        end 
+    end 
+
+    private 
+
+    def lease_params
+        params.permit(:id, :rent, :tenant_id, :lease_id)
+    end 
+
 end
